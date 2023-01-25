0b00a60980159b5eb042a376c4053a1a84833731
DomenJ <domenjelicj@gmail.com>
2023-01-25 22:09:20 +0100

    Added basic roouting logic.

---
+++ react-pwa/src/pages/404.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ForOFor() {
+    return (
+        <h1>Whoops, the page you are looking for is hiding. Try some other time</h1>
+    );
+}
+
+export default ForOFor;
