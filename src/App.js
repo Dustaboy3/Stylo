fe7a1c3e589ba21f1526dbb6661dd80cb023d630
DomenJ <domenjelicj@gmail.com>
2023-01-27 16:44:34 +0100

    test1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-// pages import 
+// pages import test1
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ForOFor from './pages/404';
-import Home from './pages/homepage';
-import Layout from './pages/layout';
+import ForOFor from "./pages/404";
+import Home from "./pages/homepage";
+import Layout from "./pages/layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout data-testid='layout'/>}>
-          <Route index element={<Home data-testid='home'/>} />
-          <Route path="*" element={<ForOFor data-testid='404'/>} />
+        <Route path="/" element={<Layout data-testid="layout" />}>
+          <Route index element={<Home data-testid="home" />} />
+          <Route path="*" element={<ForOFor data-testid="404" />} />
         </Route>
       </Routes>
     </BrowserRouter>
