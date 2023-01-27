4e9216808f09be5d119acf4c13e69d292a77579a
DomenJ <domenjelicj@gmail.com>
2023-01-27 16:14:45 +0100

    Added propTypes

--- src/components/atoms/button.jsx
+++ src/components/atoms/button.jsx
@@ -1,25 +1,56 @@
-import { Button } from '@chakra-ui/react'
-import React from 'react';
-import '../../css/atoms/header.css'
+import { Button } from "@chakra-ui/react";
+import React from "react";
+import PropTypes from "prop-types";
 
 function CustomButton(props) {
-    return (
-        <Button
-            data-testid="custom-button"
-            colorScheme={props.color}
-            size={props.size}
-            variant={props.variant}
-            leftIcon={props.lIcon}
-            rightIcon={props.rIcon}
-            _hover={{ bg: props.hover }}
-            _active={{
-                bg: props.active_bg,
-                transform: 'scale(0.90)',
-                borderColor: props.active_border,
-            }}>
-            {props.content}
-        </Button>
-    );
+  return (
+    <Button
+      data-testid="custom-button"
+      colorScheme={props.color}
+      size={props.size}
+      variant={props.variant}
+      leftIcon={props.lIcon}
+      rightIcon={props.rIcon}
+      _hover={{ bg: props.hover }}
+      _active={{
+        bg: props.active_bg,
+        transform: "scale(0.90)",
+        borderColor: props.active_border,
+      }}
+    >
+      {props.content}
+    </Button>
+  );
 }
+
+CustomButton.propTypes = {
+  color: PropTypes.shape({
+    colorScheme: PropTypes.string,
+  }),
+  size: PropTypes.shape({
+    size: PropTypes.string,
+  }),
+  variant: PropTypes.shape({
+    variant: PropTypes.string,
+  }),
+  lIcon: PropTypes.shape({
+    leftIcon: PropTypes.any,
+  }),
+  rIcon: PropTypes.shape({
+    rightIcon: PropTypes.any,
+  }),
+  hover: PropTypes.shape({
+    _hover: PropTypes.string,
+  }),
+  active_bg: PropTypes.shape({
+    _active_bg: PropTypes.string,
+  }),
+  active_border: PropTypes.shape({
+    _active_border: PropTypes.string,
+  }),
+  content: PropTypes.shape({
+    content: PropTypes.string,
+  }),
+};
 
 export default CustomButton;
